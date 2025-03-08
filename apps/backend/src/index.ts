e27757c9b0149761b6f8b4a8c11808549139bde1
slava <slava.hatnuke@gmail.com>
2025-03-08 14:43:04 +0000

    ...

--- apps/backend/src/index.ts
+++ apps/backend/src/index.ts
@@ -1,1 +1,22 @@
-console.log('hey backend');
+// src/server.ts
+import http from "http";
+import {Server} from "socket.io";
+
+const server = http.createServer((req, res) => {
+    res.writeHead(200, {"Content-Type": "text/plain"});
+    res.end("Hello from Node.js backend!");
+});
+
+const io = new Server(server);
+
+io.on("connection", (socket) => {
+    console.log("New client connected");
+
+    socket.on("disconnect", () => {
+        console.log("Client disconnected");
+    });
+});
+
+server.listen(4000, () => {
+    console.log("Server running on http://localhost:4000");
+});
