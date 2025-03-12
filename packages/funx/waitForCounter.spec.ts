c2669215168eb044271ce5d40f18628214b53b7c
slava <slava.hatnuke@gmail.com>
2025-03-12 15:14:20 +0000

    ...

--- packages/funx/waitForCounter.spec.ts
+++ packages/funx/waitForCounter.spec.ts
@@ -2,51 +2,53 @@ import { Counter } from './counter';
 import { waitForCounter, waitForZeroCounter } from './waitForCounter';
 import { delay } from './delay';
 
-import { test, expect, it } from 'vitest';
+import { describe, expect, test } from 'vitest';
 
-test('waitForCounter / initial', async function () {
-  const counter1 = Counter();
-  await waitForCounter(counter1, (counter) => counter.value() <= 0);
-});
+describe(waitForZeroCounter, () => {
+  test('waitForCounter / initial', async function () {
+    const counter1 = Counter();
+    await waitForCounter(counter1, (counter) => counter.value() <= 0);
+  });
 
-test('waitForCounter / by decrement', async function () {
-  const counter2 = Counter();
-  counter2.increment(2);
+  test('waitForCounter / by decrement', async function () {
+    const counter2 = Counter();
+    counter2.increment(2);
 
-  let resolved = false;
-  const p1 = waitForCounter(counter2, (counter) => counter.value() <= 0);
-  void p1.then(() => (resolved = true));
+    let resolved = false;
+    const p1 = waitForCounter(counter2, (counter) => counter.value() <= 0);
+    void p1.then(() => (resolved = true));
 
-  counter2.decrement();
-  await delay(100);
+    counter2.decrement();
+    await delay(100);
 
-  expect(counter2.value()).toEqual(1);
-  expect(resolved).toEqual(false);
+    expect(counter2.value()).toEqual(1);
+    expect(resolved).toEqual(false);
 
-  counter2.decrement();
-  await delay(100);
+    counter2.decrement();
+    await delay(100);
 
-  expect(counter2.value()).toEqual(0);
-  expect(resolved).toEqual(true);
-});
+    expect(counter2.value()).toEqual(0);
+    expect(resolved).toEqual(true);
+  });
 
-it(waitForZeroCounter.name, async function () {
-  const counter2 = Counter();
-  counter2.increment(2);
+  test(waitForZeroCounter.name, async function () {
+    const counter2 = Counter();
+    counter2.increment(2);
 
-  let resolved = false;
-  const p1 = waitForZeroCounter(counter2);
-  void p1.then(() => (resolved = true));
+    let resolved = false;
+    const p1 = waitForZeroCounter(counter2);
+    void p1.then(() => (resolved = true));
 
-  counter2.decrement();
-  await delay(100);
+    counter2.decrement();
+    await delay(100);
 
-  expect(counter2.value()).toEqual(1);
-  expect(resolved).toEqual(false);
+    expect(counter2.value()).toEqual(1);
+    expect(resolved).toEqual(false);
 
-  counter2.decrement();
-  await delay(100);
+    counter2.decrement();
+    await delay(100);
 
-  expect(counter2.value()).toEqual(0);
-  expect(resolved).toEqual(true);
+    expect(counter2.value()).toEqual(0);
+    expect(resolved).toEqual(true);
+  });
 });
